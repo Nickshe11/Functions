aac0044398e7e4e5e2d4518a19a0bad539d2c58a
Nickshe11 <shelobod@gmail.com>
2022-07-08 21:54:31 +0300

    Functions

--- Functions/Main.cpp
+++ Functions/Main.cpp
@@ -1,10 +1,40 @@
-//Functions
+﻿//Functions
 
 #include<iostream>
 using namespace std;
 
+int Add(int a, int b);
+int Sub(int a, int b);
+int Mul(int a, int b);
+double Div(int a, int b);
+
 void main()
 {
 	setlocale(LC_ALL, "");
-
+	int a, b;
+	cout << "Введите два числа: "; cin >> a >> b;
+	int c = Add(a, b);// Использование функции (вызов функции) function call
+	cout << a << " + " << b << " = " << c << endl;
+	cout << a << " - " << b << " = " << Sub(a, b) << endl;
+	cout << a << " * " << b << " = " << Mul(a, b) << endl;
+	cout << a << " / " << b << " = " << Div(a, b) << endl;
 }
+
+int Add(int a, int b) //Addition, реализация функции (определение функции)
+{
+	int c = a + b;
+	return c;
+}
+int Sub(int a, int b) //Subtraction (вычитание)
+{
+	return  a - b;
+}
+int Mul(int a, int b) //Multiplication
+{
+	return a * b;
+}
+double Div(int a, int b) //Division
+{
+	return (double)a / b;
+}
+
