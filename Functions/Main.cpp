cf813c437763b8e7d2730a02e785fdec1abfb7c7
Nickshe11 <shelobod@gmail.com>
2022-07-11 21:19:23 +0300

    Hometask Pow Fact

--- Functions/Main.cpp
+++ Functions/Main.cpp
@@ -7,6 +7,8 @@ int Add(int a, int b);
 int Sub(int a, int b);
 int Mul(int a, int b);
 double Div(int a, int b);
+long long Factorio(int a);
+int Power(int a, int b);
 
 void main()
 {
@@ -18,6 +20,10 @@ void main()
 	cout << a << " - " << b << " = " << Sub(a, b) << endl;
 	cout << a << " * " << b << " = " << Mul(a, b) << endl;
 	cout << a << " / " << b << " = " << Div(a, b) << endl;
+	cout << a << " в степени " << b << " = " << Power(a, b) << endl;
+	cout << a << " факториал = " << Factorio(a)<<endl;
+	cout << b << " факториал = " << Factorio(b)<<endl;
+	
 }
 
 int Add(int a, int b) //Addition, реализация функции (определение функции)
@@ -36,5 +42,21 @@ int Mul(int a, int b) //Multiplication
 double Div(int a, int b) //Division
 {
 	return (double)a / b;
+}
+long long Factorio(int a)
+{
+	long long c = 1;
+	for (int i = 1; i <= a; i++) c *= i;
+	return c;
+}
+int Power(int a, int b)
+{
+	long c = 1;
+	if (b == 0) c = 1;
+	else
+	{
+		for (int i = 0; i < b; i++)	c *= a;
+	}
+	return c;
 }
 
