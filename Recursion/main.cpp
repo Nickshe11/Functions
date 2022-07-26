43d0e0ccf7f7fc3c55991e5e51a7a9240f27335c
Nickshe11 <shelobod@gmail.com>
2022-07-26 22:10:48 +0300

    Hometask fibonachi

--- Recursion/main.cpp
+++ Recursion/main.cpp
@@ -1,15 +1,19 @@
 #include<iostream>
+#define tab "\t"
 using namespace std;
 
 void elevator(int floor);
+void fibonachi(int a, int b, int i);
 
 void main()
 {
 	setlocale(LC_ALL, "");
-	cout << "Hello world";
-	int floor;
+	//cout << "Hello world";
+	/*int floor;
 	cout << "Введите номер этажа: "; cin >> floor;
-	elevator(floor);
+	elevator(floor);*/
+	int first = 0, sec = 1, i = 0;
+	fibonachi(first, sec, i);
 }
 
 void elevator(int floor)
@@ -20,6 +24,16 @@ void elevator(int floor)
 		return;
 	}
 	cout << "Вы на " << floor << " этаже\n";
-	elevator(floor-1);
+	elevator(floor - 1);
 	cout << "Вы на " << floor << " этаже\n";
+}
+
+void fibonachi(int a, int b, int i)
+{
+	if (i == 20)
+	{
+		return;
+	}
+	cout << a << tab ; i++;
+	fibonachi(b, a + b, i);
 }
