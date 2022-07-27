a6fd75778ae51396054d82f772c7b715153ba7d4
Nickshe11 <shelobod@gmail.com>
2022-07-27 21:41:10 +0300

    Hometask recursion done

--- Recursion/main.cpp
+++ Recursion/main.cpp
@@ -3,9 +3,10 @@
 using namespace std;
 
 void elevator(int floor);
-int factorio(int num, int box, int i);
-int Power(int num, int pow, int i,int box);
-void fibonachi(int a, int b, int i, int lim);
+int factorio(int num, double box, int i);
+double Power(int num, int pow, int i, double box);
+void fibonachi(int a, int b, int i, int counter);
+void fibonachi(int a, int b, int i, double lim);
 
 void main()
 {
@@ -14,18 +15,24 @@ void main()
 	/*int floor;
 	cout << "Введите номер этажа: "; cin >> floor;
 	elevator(floor);*/
-	int num, box = 1, counter = 1;
+	int num, counter = 1;
+	double box = 1;
 	cout << "Введите число для вычисления факториала: "; cin >> num;
-	factorio(num, box, counter);
+	cout << "Факториал числа " << num << " = " << factorio(num, box, counter) << endl;
 
-	int digit, pow, i = 0;
-	cout << "Введите число для возведения в степень: "; cin >> digit;
+	int pow;
+	cout << "Введите число для возведения в степень: "; cin >> num;
 	cout << "Введите значение степени: "; cin >> pow;
-	Power(digit, pow, i, box);
+	cout << "Результат возведения числа " << num << " в степень " << pow << " = " << Power(num, pow, counter - 1, box) << endl;
 
-	int first = 0, sec = 1, limit;
-	cout << "Введите количество чисел в ряде Фибоначчи: "; cin >> limit;
-	fibonachi(first, sec, i, limit);
+	int first = 0, sec = 1, number;
+	double limit;
+	cout << "Введите количество чисел в ряде Фибоначчи: "; cin >> number;
+	fibonachi(first, sec, counter - 1, number);
+	cout << endl;
+	cout << "Введите предел для ряда Фибоначчи: "; cin >> limit;
+	fibonachi(first, sec, counter - 1, limit);
+
 }
 
 void elevator(int floor)
@@ -40,32 +47,51 @@ void elevator(int floor)
 	cout << "Вы на " << floor << " этаже\n";
 }
 
-int factorio(int num, int box, int i)
+int factorio(int num, double box, int i)
 {
 	if (i > num)
 	{
-		cout << "Факториал числа " << num << " = " << box << endl;
 		return box;
 	}
 	factorio(num, box * i, i + 1);
 }
 
-int Power(int num, int pow, int i, int box)
+double Power(int num, int pow, int i, double box)
 {
-	if (i == pow)
+	if (pow < 0)
 	{
-		cout << "Число " << num << " в степени " << pow << " = " << box<<endl;
-		return box;
+		if (i == -pow)
+		{
+			box = 1 / box;
+			return box;
+		}
+	}
+	else
+	{
+		if (i == pow)
+		{
+			return box;
+		}
 	}
 	Power(num, pow, i + 1, box * num);
 }
 
-void fibonachi(int a, int b, int i, int lim)
+void fibonachi(int a, int b, int i, int counter)
 {
-	if (i == lim)
+	if (i == counter)
 	{
 		return;
 	}
 	cout << a << tab;
+	fibonachi(b, a + b, i + 1, counter);
+}
+
+void fibonachi(int a, int b, int i, double lim)
+{
+	cout << a << tab;
+	if (b > lim)
+	{
+		return;
+	}
 	fibonachi(b, a + b, i + 1, lim);
 }
